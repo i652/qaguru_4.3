70fba2920c7c18c0a0248c237b7fcd3e4a6cc367
egor <egor@onet.eu>
2021-02-18 22:50:58 +0300

    Allure steps and description

--- src/test/java/AllureStepsAdd.java
+++ src/test/java/AllureStepsAdd.java
@@ -1,6 +1,5 @@
 import com.codeborne.selenide.Condition;
-import io.qameta.allure.Owner;
-import io.qameta.allure.Step;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
@@ -13,26 +12,31 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class AllureStepsAdd {
+    private static final String
+            BASEURL = "https://github.com/",
+            OWNER = "i652",
+            REPOSITORY = "/qaguru_4.3";
+
     @Test
+    @Link(BASEURL + OWNER + REPOSITORY)
     @Tags({@Tag("web"), @Tag("critical")})
-    @Owner("i652")
-    @DisplayName("Поиск репозитория qaguru_4.3 у автора i652")
+    @Feature("Contributors")
+    @Story("Поиск авторов в репозитории")
+    @Owner(OWNER)
+    @DisplayName("Поиск репозитория " + REPOSITORY + " у автора " +OWNER)
     void AllureBaseSteps() {
-        final String
-                author = "i652",
-                repo = author + "/qaguru_4.3";
         step("Открываем Гитхаб", ()-> {
-            open("https://github.com/");
+            open(BASEURL);
         });
 
-        step("Ищем репозиторий " + repo, ()-> {
+        step("Ищем репозиторий " + REPOSITORY, ()-> {
             $(".header-search-input").click();
-            $(".header-search-input").sendKeys(repo);
+            $(".header-search-input").sendKeys(OWNER + REPOSITORY);
             $(".header-search-input").submit();
         });
 
-        step("Переходим в репозиторий " + repo, ()-> {
-            $(byLinkText(repo)).click();
+        step("Переходим в репозиторий " + REPOSITORY, ()-> {
+            $(byLinkText(OWNER + REPOSITORY)).click();
         });
 
         step("Переходим в раздел Insights", ()-> {
@@ -43,9 +47,8 @@ public class AllureStepsAdd {
             $(withText("Contributors")).click();
         });
 
-        step("Убеждаемся, что автор " + author, ()-> {
-            $(withText(author)).should(Condition.exist);
+        step("Убеждаемся, что автор " + OWNER, ()-> {
+            $(withText(OWNER)).should(Condition.exist);
         });
-        sleep(1000);
     }
 }
